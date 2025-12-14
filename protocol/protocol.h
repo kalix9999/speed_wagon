02cef690958853c32d183cc5a245c0291cb93624
kalix <114808452+kalix9999@users.noreply.github.com>
2025-12-15 01:21:47 +0900

    Update protocol.h

--- protocol/protocol.h
+++ protocol/protocol.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+
+typedef struct __attribute__((packed)){
+    int32_t speed;
+    int32_t is_overspeed;
+}Packet;
+
 
