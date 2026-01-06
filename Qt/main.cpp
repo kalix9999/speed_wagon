51a380dc4e09b0f1fa4fd7df93e6a6b22927a73c
kalix9999 <gmlckd343621@gmail.com>
2026-01-06 19:50:35 +0900

    QT

---
+++ Qt/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include "Client.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //MainWindow w;
+   // w.show();
+    Client c;
+    c.show();
+    return a.exec();
+}
